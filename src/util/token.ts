2b2089fcc7ce8612cbae4e8fba0b99d4668b83ea
Peerawat Poombua <peerawat.pm@gmail.com>
2023-04-29 11:21:04 +0700

    rename folder

---
+++ src/util/token.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const secretKey = "supersecret";
+
+export const generateToken = ({ id }: { id: string }) => {
+  return jwt.sign({ id }, secretKey, { expiresIn: "1d" });
+};
+
+export const verifyToken = (token: string) => {
+  return <jwt.MyJwtPayload>jwt.verify(token, secretKey);
+};
+
+declare module "jsonwebtoken" {
+  export interface MyJwtPayload extends JwtPayload {
+    id: string;
+  }
+}
